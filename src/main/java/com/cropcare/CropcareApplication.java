c6ee993379d104e8ee7e3e6ab0f3199f27d3175f
Sanket-378 <sanketkhatkale07@gmail.com>
2026-04-21 18:51:38 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/cropcare/CropcareApplication.java
@@ -0,0 +1,13 @@
+package com.cropcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CropcareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CropcareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cropcare/CropcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cropcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CropcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
