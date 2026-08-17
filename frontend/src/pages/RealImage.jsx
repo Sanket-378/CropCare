9bc6e2893c66b8f6c58151bb32456b2245b583ed
Sanket-378 <sanketkhatkale07@gmail.com>
2026-08-17 19:47:11 +0530

    Connect disease detection to deployed API

--- frontend/src/pages/RealImage.jsx
+++ frontend/src/pages/RealImage.jsx
@@ -128,7 +128,7 @@ function RealImage() {
 
       const response = await axios.post(
 
-        "http://127.0.0.1:5000/predict",
+        "https://cropcare-disease.onrender.com/predict",
 
         formData,
 
